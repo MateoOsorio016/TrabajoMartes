34032e9f4dffee0c410c6c0423c4897b39a6a48d
MateoOsorio016 <mosorio7347@misena.edu.co>
2023-09-11 07:32:32 -0500

    tercer commit

--- lib/home_screen/login.dart
+++ lib/home_screen/login.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/home_screen/lista.dart';
 
-
 class Login extends StatefulWidget {
   const Login({super.key});
 
   @override
   State<Login> createState() => _LoginState();
-
 }
+
 class _LoginState extends State<Login> {
   final _formKey = GlobalKey<FormState>();
   TextEditingController emailController = TextEditingController();
@@ -22,67 +21,88 @@ class _LoginState extends State<Login> {
       ),
       body: Form(
         key: _formKey,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                child: TextFormField(
-                  controller: emailController,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(), labelText: "Email"),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your email';
-                    }
-                    return null;
-                  },
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 255, 252, 251), // Fondo gris
+              ),
+            ),
+            Center(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Image.asset(
+                        '../asset/burdeo_logo.png', // Ruta de tu imagen de logo
+                        height: 120,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: TextFormField(
+                        controller: emailController,
+                        style: TextStyle(color: Colors.red), // Texto en rojo
+                        decoration: const InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white, // Fondo blanco
+                            border: OutlineInputBorder(),
+                            labelText: "Email"),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your email';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: TextFormField(
+                        controller: passwordController,
+                        style: TextStyle(color: Colors.red), // Texto en rojo
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white, // Fondo blanco
+                            border: OutlineInputBorder(),
+                            labelText: "Password"),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your password';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            final route = MaterialPageRoute(
+                                builder: (context) => const MyList());
+                            Navigator.push(context, route);
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                  content: Text('Please fill input')),
+                            );
+                          }
+                        },
+                        child: const Text('Submit'),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                child: TextFormField(
-                  controller: passwordController,
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(), labelText: "Password"),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your password';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16.0),
-                child: Center(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        final route = MaterialPageRoute(builder: (context) => const MyList());
-                        Navigator.push(context, route);
-                      } else {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(content: Text('Please fill input')),
-                        );
-                      }
-                    },
-                    child: const Text('Submit'),
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
+      backgroundColor: Color.fromARGB(255, 255, 250, 250), // Fondo gris
     );
   }
 }
-
-
