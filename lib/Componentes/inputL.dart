c64eeffc942e83300af16be14a8694f9e1c6eea7
unknown <SENA@MEDAPRCSGFSP537.FormacionSena.RED>
2023-09-20 10:22:52 -0500

    Login

---
+++ lib/Componentes/inputL.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class InputComponent extends StatelessWidget {
+  const InputComponent({
+    Key? key,
+    required this.label,
+    required this.controller,
+    this.value,
+    this.obscureText = false,
+    this.keyboardType = TextInputType.text,
+    this.validator
+  }) : super(key: key);
+
+  final String label;
+  final TextEditingController controller;
+  final String? value;
+  final bool obscureText;
+  final TextInputType keyboardType;
+  final String? Function(String?)? validator;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      initialValue: value == '' ? null : value,
+      controller: controller,
+      obscureText: obscureText,
+      keyboardType: keyboardType,
+      decoration: InputDecoration(
+        labelText: label,
+        border: const OutlineInputBorder(),
+      ),
+      validator: validator,
+    );
+  }
+}
